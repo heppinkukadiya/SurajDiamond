2bb03a774dafb42edb8f660bd87dfddf96424b01
heppinkukadiya <heppinkukadiya@gmail.com>
2025-05-28 23:25:33 +0530

    index

--- backend/index.js
+++ backend/index.js
@@ -3,28 +3,26 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
-
 const app = express();
 
 app.use(express.json());
 
 app.use(
     cors({
-        origin:"http://localhost:3000",
-        credentials:true,
+        origin: "http://localhost:3000",
+        credentials: true,
     })
-)
+);
 
 const routes = require('./Routes/routes');
 app.use("/api", routes);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
-    });
-}
+app.use(express.static(path.join(__dirname, "..", 'build')));
 
+// Catch-all route to serve the React app for any unknown routes except /api routes
+app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "..", 'build', 'index.html'));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
